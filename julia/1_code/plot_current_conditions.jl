f26dbd61a3bcff43cc93b3a1c0f13b049286ffc5
br0wnelvis <122317868+br0wnelvis@users.noreply.github.com>
2023-01-11 19:02:01 +0000

    added quick plotting module

--- julia/1_code/plot_current_conditions.jl
+++ julia/1_code/plot_current_conditions.jl
@@ -1,10 +1,11 @@
 
 include("problemConditionStruct.jl");
 using LegendrePolynomials: collectPl
+using Plots;
 """
     plot_current_conditions(Union{ProblemConditions, Vector{Float64}})
 
-Plots the current conditions of the 
+Plots the current conditions of the problem
 """
 function plot_current_conditions(data::Union{ProblemConditions, Vector{Float64}}; center_of_mass::Float64 = NaN);
 
@@ -14,8 +15,23 @@ function plot_current_conditions(data::Union{ProblemConditions, Vector{Float64}}
         end
         data = data.deformation_amplitudes; 
     end
+    if isnan(center_of_mass); center_of_mass = 1+sum([(-1)^ii * data[ii] for ii in eachindex(data)]); end
+    d = length(data);
+    ζ(θ::Float64) = sum(data .* (collectPl(cos(θ), lmax = d).parent)[2:end]);
+    x(θ::Float64) = sin(θ) * (1 + ζ(θ));
+    y(θ::Float64) = center_of_mass + cos(θ) * (1 + ζ(θ));
 
-    
+    rrange = LinRange(0, 2π, 500);
 
-
+    plot(
+        x.(rrange), y.(rrange), 
+        lims = (-2, 2),
+        lw = 2
+    )
+    default(show = true)
+    xlims!(-2, 2)
+    ylims!(0, 3)
 end
+
+plot_current_conditions([0.0, 0.1])
+
