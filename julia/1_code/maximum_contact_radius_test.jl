fadb717eaaf183c1e88c039c1c4ec5d08d0460fb
br0wnelvis <122317868+br0wnelvis@users.noreply.github.com>
2023-01-11 20:26:51 +0000

    added nb contact time unit testing

--- julia/1_code/maximum_contact_radius_test.jl
+++ julia/1_code/maximum_contact_radius_test.jl
@@ -4,6 +4,7 @@ module tester
     using ReTest
     include("./maximum_contact_radius.jl")
     include("./zeta.jl");
+    include("./r_from_spherical.jl");
 
     function manual_max_radius(amplitudes; tol = 1e-5)
         # using bissection method
@@ -11,25 +12,27 @@ module tester
         # ζ(θ::Float64)::Float64 = sum(amplitudes .* (collectPl(cos(θ), lmax = order))[2:end])
         ζ = zeta(amplitudes; order = order);
         drdθ(θ::Float64)::Float64 = cos(θ) * (1 + ζ(θ)) - 
-            sin(θ)^2 * sum(amplitudes .* (collectdnPl(cos(θ), lmax = order, n = 1))[2:end]);
+            sin(θ)^2 * sum(amplitudes .* (collectdnPl(cos(θ), lmax = order, n = 1))[1:end]);
         
         n = 1;
         θ_min = pi/2-pi/8;
-        θ_max = pi;
-        @assert drdθ(θ_min) < 0 "Bad guess on θ_min!"
-        @assert drdθ(θ_max) ≥ 0 "Bad guess on θ_max!"
-        while b-a > tol && n < 200
+        θ_max = 3.14159;
+        while drdθ(θ_min) < 0 
+            θ_min = rand() * pi/2;
+        end
+        @assert drdθ(θ_max) ≤ 0 "Bad guess on θ_max!"
+        while θ_max-θ_min > tol && n < 200
             θ_mid = (θ_min+θ_max)/2;
             if abs(drdθ(θ_mid)) < tol
                 return r_from_spherical(θ_mid, amplitudes);
             elseif drdθ(θ_mid) > 0
-                θ_max = θ_mid;
-            elseif drdθ(θ_mid) < 0
                 θ_min = θ_mid;
+            elseif drdθ(θ_mid) < 0
+                θ_max = θ_mid;
             end
              n += 1;
         end
-        throw("Couldn't converge!: $r, $amplitudes")
+        throw("Couldn't converge!: $amplitudes, $θ_max")
     end
     # println(manual_max_radius)
     function test_amplitudes(range, amplitudes)
@@ -42,12 +45,17 @@ module tester
         @test maximum_contact_radius([0.0, 0.0]) ≈ 1 rtol=1e-5
         # test_amplitudes(range(0.0, 0.99, length=15), [0, 0]);
     end
-    """
-    @testset "Amplitudes 0, 0.1" begin
-        amp = [0.0, 0.1];
-        test_amplitudes(range(0.0, 0.7, length = 5), amp);
+    
+    @testset "Amplitudes .1, -.1, .4, .3" begin
+        amp = [.1, -.1, .4, .3];
+        @test maximum_contact_radius(amp) ≈ manual_max_radius(amp) atol=1e-4
     end
-    """
+
+    @testset "Amplitudes  0.15 * (.2, -.1, .4, -.3)" begin
+        amp = 0.15 .* [.2, -.1, .4, -.3];
+        @test maximum_contact_radius(amp) ≈ manual_max_radius(amp) atol=1e-4
+    end
+    
 
 end
 
