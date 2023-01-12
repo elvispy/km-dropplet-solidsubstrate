de272a841c4b40f5ff24db680affe23ec231405d
Alex <elvisavfc65@gmail.com>
2023-01-12 10:30:28 -0500

    simple testing plotting

--- julia/1_code/test.jl
+++ julia/1_code/test.jl
@@ -1,19 +1,13 @@
-using LegendrePolynomials, OffsetArrays
+using Plots
+gr()
+t = range(0, 4π, length = 100);
+r = range(1, 0, length = 100);
 
-A = collectPl(0.5, lmax = 5)
-amplitudes = [0.0, 1, 2, 3, 4, 5];
+x = cos.(t) .* r;
+y = sin.(t) .* r;
 
-
-function broadcast(f::Any, amplitudes::Vector{Float64}, A::T) where T <: OffsetArrays.OffsetArray 
-    result = fill(0.0, length(amplitudes));
-    for (ii, jj) in zip(eachindex(amplitudes), eachindex(A))
-        result[ii] = f(amplitudes[ii], A[jj]); 
-    end
-
-    return result
+@gif for i in eachindex(x)
+    scatter(x[i], y[i], lims = (-1, 1), label = "")
 end
-
-
-#.+(amplitudes::Vector{Floag64}, A::T) where T <: OffsetArrays.OffsetArray = broadcast(+, amplitudes, A)
-
-println(amplitudes .+ A)
+println("Hey!")
+readline()
